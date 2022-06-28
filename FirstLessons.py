2405eb9541437dffd9769e58054c61a8eeeb4a73
charlesdev771 <charlesdev0721@protonmail.com>
2022-06-27 22:14:46 -0300

    First day

--- FirstLessons.py
+++ FirstLessons.py
@@ -1,1 +1,16 @@
-print("Hello World")
+#This is a commentary :)
+'''
+    This is another commentary (:
+        types
+            srt
+            int
+            float
+            bool
+'''
+print('Jooj')
+print(type('Jooj'), 'aa', sep='-', end='#\n')
+
+age = 5.0
+
+age_question = age > 18
+print(f'{age_question:.5f}')
